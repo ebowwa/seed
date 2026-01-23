79ddfc139972de7ca2b47f1d419bec413ac5dd6a
ebowwa <ec.arbee1@gmail.com>
2026-01-23 13:22:44 -0800

    Revert "feat: add Ralph Iterative plugin installer"
    This reverts commit ec471d0a44486893b61b476823250a80cb5780ac.

--- v2/src/tools/registry.ts
+++ v2/src/tools/registry.ts
@@ -13,7 +13,6 @@ import { GhTool } from "./gh";
 import { DopplerTool } from "./doppler";
 import { NodeTool } from "./node";
 import { TmuxTool } from "./tmux";
-import { RalphTool } from "./ralph";
 
 export interface Tool {
   name: string;
@@ -55,7 +54,6 @@ export class ToolRegistry {
       new LaneTool(),
       new GhTool(),
       new DopplerTool(),
-      new RalphTool(),
     ];
   }
 
